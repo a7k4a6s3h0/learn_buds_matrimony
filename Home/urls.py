87f96f1939177ed07fb692980fa2ff1ef88eb56c
anfiquehussain <adomax2003@gmail.com>
2024-08-07 18:33:32 +0530

    location,qualifaction,location page created,font-updated

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('qualification', views.Qualification.as_view(), name='qualification'),
+    path('loaction', views.Loaction.as_view(), name='loaction'),
+    path('designation', views.Designation.as_view(), name='designation'),
 ]
