0a17083ee560cb8e4e558b962860a99b04c22428
vishnuvgd <vishnudaskallingal7@gmail.com>
2024-08-10 23:31:39 +0530

    subscription

--- subscription/urls.py
+++ subscription/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = []
+urlpatterns = [
+    path('subscribe',views.SubscriptionView.as_view(),name='subscribe'),
+    path('addpayment',views.AddPaymentView.as_view(),name='addpayment'),
+    path('addcard',views.AddCardView.as_view(),name='addcard'),
+]
