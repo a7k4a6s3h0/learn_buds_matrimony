9d2e8413ea0069e7027c11e3a4dcab546aa29101
vishnuvgd <vishnudaskallingal7@gmail.com>
2024-08-10 23:31:39 +0530

    subscription

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class AddPaymentView(TemplateView):
+    template_name='AddPayment.html'
+    
+class SubscriptionView(TemplateView):
+    template_name='Subscription.html'
+    
+class AddCardView(TemplateView):
+    template_name='Addcard.html'
