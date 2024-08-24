149535dc717109c3d378669b1cb279b929880cc0
a7k4a6s3h0 <akashsuresh267@gmail.com>
2024-08-24 19:08:03 +0530

    implement user home backend code

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from U_auth.permissions import RedirectAuthenticatedUserMixin, RedirectNotAuthenticatedUserMixin
+from django.views.generic.edit import FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 # Create your views here.
-class Home(TemplateView):
+class Home(RedirectNotAuthenticatedUserMixin, TemplateView):
     template_name='Home/home.html'
 
 
