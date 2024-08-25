80946dc48224bf354b21e2e6efa47eb00410010c
Alan-An <alananilkumar125@gmail.com>
2024-08-25 20:11:04 +0530

    images file name changes

--- matrimony_admin/urls.py
+++ matrimony_admin/urls.py
@@ -3,5 +3,5 @@ from Home.urls import urlpatterns
 from . import views
 
 urlpatterns =[
-    
+    path('admin_home', views.admin_home, name="admin_home"),
 ]
--- matrimony_admin/views.py
+++ matrimony_admin/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def admin_home(request):
+    return render(request, "admin/admin_home.html")
