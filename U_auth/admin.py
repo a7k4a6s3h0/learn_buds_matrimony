6e21d227b282ea7d5b379842064cdefc84d79e0c
a7k4a6s3h0 <akashsuresh267@gmail.com>
2024-08-27 12:14:20 +0530

    auth view updated

--- U_auth/admin.py
+++ U_auth/admin.py
@@ -7,4 +7,14 @@ admin.site.register(Country_codes)
 admin.site.register(languages)
 admin.site.register(OTP)
 
+admin.site.register(UserPersonalDetails)
+admin.site.register(Pictures)
+admin.site.register(Hobbies)
+admin.site.register(Intrestes)
+admin.site.register(Job_Details)
+admin.site.register(Relationship_Goals)
+admin.site.register(AdditionalDetails)
+admin.site.register(UserDisabilities)
+
+
 
