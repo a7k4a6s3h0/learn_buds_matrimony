d833004c5e251e9170ed08294ebd4a06a7e99979
anfiquehussain <adomax2003@gmail.com>
2024-09-04 12:48:50 +0530

    notifiction management created

--- matrimony_admin/urls.py
+++ matrimony_admin/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns =[
     path('admin/home', views.admin_home, name="admin_home"),
-    path('user/', views.usr_mng, name="user_management")
+    path('user/', views.usr_mng, name="user_management"),
+    path('notification_management',views.NotifcationManagement.as_view(),name='notification_management')
 ]
