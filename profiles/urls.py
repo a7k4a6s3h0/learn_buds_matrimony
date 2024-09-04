c3d9dd658039a156f0e6cefd0572863a890c49f8
Alan-An <alananilkumar125@gmail.com>
2024-09-04 17:34:15 +0530

    send request, view sented and received request

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,11 @@ from . import views
 urlpatterns = [
     path('profile/<int:user_id>/',views.demo_pr, name="profile"),
     path('msg_pg',views.messages_pg, name="msg_pg"),
+    path('sent/<int:pk>/',views.SentedRequestView.as_view(), name="send_request"),
+    path('sent/',views.SentedRequestView.as_view(), name="sented_request"),
+    path('accept/',views.AcceptedRequestView.as_view(), name="accepted_request"),
+    path('received/',views.ReceivedRequestView.as_view(), name="received_request"),
+    path('request-hanlde/<int:pk>/<str:action>/', views.RequestHandleView.as_view(), name='request_handle'),
     # path('send',views.user_send_pg, name="send"),
     # path('accept',views.user_accept_pg, name="accept"),
     # path('reject',views.user_reject_pg, name="reject"),
