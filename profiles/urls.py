1db048039889e54d0be548b45fcece8c1e337cfd
Alan-An <alananilkumar125@gmail.com>
2024-09-09 15:39:58 +0530

    commit

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('profile/<int:user_id>/',views.UserProfileView.as_view(), name="profile"),
+    # path('msg_pg',views.messages_pg, name="msg_pg"),
     path('send/<int:pk>/',views.SendRequestView.as_view(), name="send_request"),
     path('sent/',views.SentedRequestView.as_view(), name="sented_request"),
     path('accept/',views.AcceptedRequestView.as_view(), name="accepted_request"),
