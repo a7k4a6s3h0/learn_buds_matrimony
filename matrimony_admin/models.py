8fe6bce10187690e4e1a2ff48315cd4948fd0907
a7k4a6s3h0 <akashsuresh267@gmail.com>
2024-09-15 11:07:21 +0530

    make changes in subscribtion

--- matrimony_admin/models.py
+++ matrimony_admin/models.py
@@ -1,3 +1,61 @@
+from datetime import timedelta
 from django.db import models
-
+from U_auth.models import costume_user
+from django.utils.text import slugify
 # Create your models here.
+
+
+class Subscription(models.Model):
+
+    PLAN_CHOICES = [
+        ('free', 'Free'),
+        ('Weekly', 'Weekly'),
+        ('Monthly', 'Monthly'),
+        ('premium', 'Premium'),
+    ]
+
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+    ]
+
+    slug = models.SlugField(unique=True, blank=True, null=True)  # New slug field
+    plan_type = models.CharField(max_length=100, choices=PLAN_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(blank=False)
+    end_date = models.DateField(blank=True)  # Manually set the end date based on the plan duration
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Subscription"
+        verbose_name_plural = "Subscriptions"
+
+    def __str__(self):
+        return self.plan_type
+
+    # Optional method to calculate end date based on the subscription duration
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.plan_type)
+
+        if not self.end_date:
+            # Example: Set end_date to 30 days from start_date for premium plans
+            self.end_date = self.start_date + timedelta(days=30 if self.plan_type == 'premium' else 0)
+        super().save(*args, **kwargs)
+
+
+class SubscriptionINFO(models.Model):
+
+    sub = models.ForeignKey(Subscription, related_name="sub_info", on_delete=models.CASCADE)
+    info = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "SubscriptionINFO"
+        verbose_name_plural = "SubscriptionINFOS"
+
+    def __str__(self):
+        return f"{self.sub.plan_type}_info"
--- subscription/views.py
+++ subscription/views.py
@@ -1,14 +1,21 @@
 # views.py
 from typing import Any
 from urllib import request
+
+from django.http import HttpRequest, JsonResponse
+from django.http.response import HttpResponse as HttpResponse
 import razorpay
 from django.conf import settings
 from django.views.generic import FormView, TemplateView
 from django.urls import reverse_lazy
 from .forms import PaymentForm
 from .models import Payment
-from django.http import JsonResponse
+from django.urls import reverse
+
+from matrimony_admin.models import Subscription, SubscriptionINFO
+
 from U_auth.permissions import RedirectNotAuthenticatedUserMixin
+from http import HTTPStatus
 
 class PaymentView(RedirectNotAuthenticatedUserMixin,FormView):
     template_name = 'Addcard.html'
@@ -29,9 +36,13 @@ class PaymentView(RedirectNotAuthenticatedUserMixin,FormView):
             'payment_capture': '1'
         })
 
+        print(razorpay_order,"order details")
+
         # Save the order details in the database
         payment = Payment.objects.create(
             user = self.request.user,
+            payment_type = 'razorpay',
+            subscription_plan = Subscription.objects.get(id=1),
             razorpay_order_id=razorpay_order['id'],
             amount=form.cleaned_data['amount'],
             status='created'
@@ -49,16 +60,8 @@ class PaymentView(RedirectNotAuthenticatedUserMixin,FormView):
         return JsonResponse(context)
     
 class PaymentCallbackView(RedirectNotAuthenticatedUserMixin,TemplateView):
-    template_name = 'payment_status.html'
+    template_name = 'Addcard.html'
     
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs) 
-        
-        context["payment_details"] = Payment.objects.get(user=self.request.user)
-        # print(context)
-        return context
-    
-
     def post(self, request, *args, **kwargs):
         client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
 
@@ -74,26 +77,59 @@ class PaymentCallbackView(RedirectNotAuthenticatedUserMixin,TemplateView):
             'razorpay_signature': razorpay_signature
         }
 
+        payment = Payment.objects.get(razorpay_order_id=razorpay_order_id)
+
         try:
             # Signature verification
             client.utility.verify_payment_signature(params_dict)
-            payment = Payment.objects.get(razorpay_order_id=razorpay_order_id)
             payment.razorpay_payment_id = razorpay_payment_id
             payment.razorpay_signature = razorpay_signature
-            payment.status = 'successful'
+            payment.status = HTTPStatus.OK
             payment.save()
             context = {'status': 'Payment Successful'}
         except:
+            payment.status = HTTPStatus.BAD_REQUEST
+            payment.save()
             context = {'status': 'Payment Failed'}
 
+        print(context,"in PaymentCallbackView post method")
         return self.render_to_response(context)
+        # return reverse('')
     
-class PaymentSuccess(TemplateView):
+class PaymentSuccess(RedirectNotAuthenticatedUserMixin,TemplateView):
     template_name='payment_success.html'
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs) 
+        print("in context")
+        context["payment_details"] = Payment.objects.filter(user=self.request.user)
+        print(Payment.objects.filter(user=self.request.user))
+        # print(context)
+        return context
+    
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        return super().get(request, *args, **kwargs)
     
 class SubscriptionView(TemplateView):
     template_name='Subscribe.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+        subscription_details = Subscription.objects.all()
+        for su in subscription_details:
+            print(su.sub_info.all())
+        context['subscription_details'] = subscription_details
+
+        return context
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        previous_url = request.META.get('HTTP_REFERER', '/default-url/')
+        # next_url = request.GET.get('next', '/default-url/')  # If 'next' not provided, use default
+        print(previous_url, "url.....")
+        return super().get(request, *args, **kwargs)
     
 class AddPaymentView(TemplateView):
     template_name='AddPayment.html'
+
     
