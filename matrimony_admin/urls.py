b018f3af48aa28898c830521ef07774e5b78ad9b
Alan-An <alananilkumar125@gmail.com>
2024-09-20 20:34:57 +0530

    admin home

--- matrimony_admin/urls.py
+++ matrimony_admin/urls.py
@@ -3,7 +3,7 @@ from Home.urls import urlpatterns
 from . import views
 
 urlpatterns =[
-    path('admin/home', views.admin_home, name="admin_home"),
+    path('home', views.admin_home, name="home"),
     path('user/', views.usr_mng, name="user_management"),
     path('notification_management',views.NotifcationManagement.as_view(),name='notification_management')
 ]
